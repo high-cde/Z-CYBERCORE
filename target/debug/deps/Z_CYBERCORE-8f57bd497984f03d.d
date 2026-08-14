a311b1d09a0410945a01ac64171ee1d261a0e391
high-cde <high-cde@z-core.network>
2026-08-14 19:49:54 +0200

    🔧 FIX: Nomenclatura Rust allineata e pulizia file di build. AGI Online.

--- target/debug/deps/Z_CYBERCORE-8f57bd497984f03d.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/Z-CYBERCORE/target/debug/deps/Z_CYBERCORE-8f57bd497984f03d.d: src/main.rs
-
-/data/data/com.termux/files/home/Z-CYBERCORE/target/debug/deps/Z_CYBERCORE-8f57bd497984f03d: src/main.rs
-
-src/main.rs:
